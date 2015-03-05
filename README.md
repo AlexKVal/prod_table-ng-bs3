d26072867c91f3a27d46751976872e29a4fab894
AlexKVal <alexkval@gmail.com>
2015-03-05 16:56:41 +0200

    fork

---
+++ README.md
@@ -0,0 +1,16 @@
+#### It's an AngularJS version of [AlexKVal/react-thinking](https://github.com/AlexKVal/react-thinking/)
+
+Frameworks used:
+- Bootstrap (CSS only)
+- AngularJS
+
+In console run:
+
+    $ git clone https://github.com/AlexKVal/products-bs3
+    $ cd products-bs3
+    $ bower install
+    $ python -m SimpleHTTPServer 8000 & open http://localhost:8000
+
+Result should look like this:
+
+![](https://raw.githubusercontent.com/AlexKVal/react-thinking/images/images/result.png)
---
+++ code.js
@@ -0,0 +1,63 @@
+(function() {
+
+  angular.module('productsTable', [])
+
+  .directive('filterableTable', function(){
+    return {
+      restrict: 'E',
+      replace: true,
+      transclude: true,
+      template: "<div class='well filterable-table' ng-transclude></div>"
+    };
+  })
+
+  .directive('productsTable', function(Product){
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'templates/directives/products-table.html',
+      link: function(scope, element, attrs){
+        scope.categories = [];
+
+        var lastCategory = {name: null};
+        Product.all().forEach(function(product){
+          if (lastCategory.name !== product.category) { // new category
+            var newCategory = { name: product.category, products: [product]};
+
+            scope.categories.push(newCategory);
+            lastCategory = newCategory;
+          } else {
+            lastCategory.products.push(product);
+          }
+        });
+        // console.log(JSON.stringify(scope.categories));
+      }
+    };
+  })
+
+  .directive('searchBar', function(){
+    return {
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'templates/directives/search-bar.html'
+    };
+  })
+
+  .factory('Product', function ProductFactory(){
+    return {
+      all: function() {
+        return glVarData;
+      }
+    };
+  });
+
+
+  var glVarData = [
+    {category: "Sporting Goods", price: "$49.99", stocked: true, name: "Football"},
+    {category: "Sporting Goods", price: "$9.99", stocked: true, name: "Baseball"},
+    {category: "Sporting Goods", price: "$29.99", stocked: false, name: "Basketball"},
+    {category: "Electronics", price: "$99.99", stocked: true, name: "iPod Touch"},
+    {category: "Electronics", price: "$399.99", stocked: false, name: "iPhone 5"},
+    {category: "Electronics", price: "$199.99", stocked: true, name: "Nexus 7"}
+  ];
+})();
