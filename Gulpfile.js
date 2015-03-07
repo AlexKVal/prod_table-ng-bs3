da9dd37d0351d25a623100ea4a5ca3821345b582
AlexKVal <alexkval@gmail.com>
2015-03-07 19:28:12 +0200

    refactor gulp task. this is a working version.

--- Gulpfile.js
+++ Gulpfile.js
@@ -6,15 +6,11 @@ var gulp = require('gulp'),
     mainBowerFiles = require('main-bower-files'),
     del = require('del');
 
-gulp.task('clean', function () {
-  del(['public']);
-});
-
 gulp.task('bower', function () {
   return bower();
 });
 
-gulp.task('bootstrap', function () {
+gulp.task('bootstrap', ['bower'], function () {
   gulp.src('bower_components/bootstrap/dist/css/*.min.css')
   .pipe(vendor('bs3.min.css'))
   .pipe(gulp.dest('public/css'));
@@ -23,27 +19,50 @@ gulp.task('bootstrap', function () {
   .pipe(gulp.dest('public/fonts'));
 });
 
-gulp.task('vendor_js', function () {
-  gulp.src(mainBowerFiles())
-  .pipe(gulp.dest('public/js'));
-});
-
-gulp.task('style', function(){
+gulp.task('style', ['bootstrap'], function(){
   gulp.src('src/css/*.css')
   .pipe(gulp.dest('public/css'));
 });
 
-gulp.task('connect', function () {
+gulp.task('vendor_js', ['bower'], function () {
+  gulp.src(mainBowerFiles())
+  .pipe(gulp.dest('public/js'));
+});
+
+gulp.task('js', ['vendor_js'], function () {
+  gulp.src('src/*.js')
+  .pipe(gulp.dest('public'));
+});
+
+gulp.task('templates', function(){
+  gulp.src('src/templates/**/*')
+  .pipe(gulp.dest('public/templates'));
+});
+
+gulp.task('html', ['templates'], function(){
+  gulp.src('src/*.html')
+  .pipe(gulp.dest('public'));
+});
+
+gulp.task('connect', ['compile'], function () {
   connect.server({
+    root: 'public',
     livereload: true
   });
 });
 
-gulp.task('open_in_browser', shell.task([
+gulp.task('open_in_browser', ['connect'], shell.task([
   'open http://localhost:8080'
 ]));
 
-gulp.task('default', ['bower', 'clean',
-  'bootstrap', 'style',
-  'connect', 'open_in_browser'
+gulp.task('compile', [
+  'style',
+  'js',
+  'html'
 ]);
+
+gulp.task('clean', function () {
+  del(['public']);
+});
+
+gulp.task('default', ['open_in_browser']);
